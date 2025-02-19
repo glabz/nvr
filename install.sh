b3f1ff5901fb75ce7758c81e4a59a2f3435a8032
Gabe Westmont <gabe@westmontweb.net>
2025-02-19 16:47:17 -0600

    update frigate version

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ USER_HOME=$(eval echo ~$USER)
 FRIGATE_HOME=$USER_HOME/.frigate
 
 mkdir -p $FRIGATE_HOME
+cp docker-compose.yml $FRIGATE_HOME/docker-compose.yml
 
 set -e
-cd $FRIGATE_HOME
+pushd $FRIGATE_HOME
 
 echo "Setting permissions on $FRIGATE_HOME"
 chown -R $USER $FRIGATE_HOME || true
@@ -17,8 +18,8 @@ set +e
 
 echo "docker compose down"
 sudo -u $USER docker compose down 2> /dev/null
-echo "docker compose rm -rf"
-sudo -u $USER docker rm -f /scrypted /scrypted-watchtower 2> /dev/null
+#echo "docker compose rm -rf"
+#sudo -u $USER docker rm -f /scrypted /scrypted-watchtower 2> /dev/null
 
 set -e
 
@@ -27,3 +28,5 @@ sudo -u $USER docker compose pull
 
 echo "docker compose up -d"
 sudo -u $USER docker compose up -d
+
+popd
